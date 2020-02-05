0a277cc2a544455128a94244a2f1158daece4edb
Tony Griffin <tony@madetech.com>
2020-02-05 16:41:50 +0000

    Add log4j, messagedriven bean skeleton

--- myejb/src/main/java/SomethingHilarious.java
+++ myejb/src/main/java/SomethingHilarious.java
@@ -1,8 +1,33 @@
-import javax.ejb.Stateless;
+import javax.ejb.*;
+import javax.jms.Message;
+import javax.jms.MessageListener;
 
-@Stateless
-public class SomethingHilarious {
+import org.apache.log4j.Logger;
+
+@MessageDriven(name = "something", activationConfig = {
+  @ActivationConfigProperty(propertyName = "destination", propertyValue = "JMS/myActivationSpec"),
+  @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+
+})
+public class SomethingHilarious implements MessageDrivenBean, MessageListener {
+  final static Logger logger = Logger.getLogger(SomethingHilarious.class);
 
   public SomethingHilarious() {
+    logger.error("Oh NOOOO!");
+  }
+
+  @Override
+  public void setMessageDrivenContext(MessageDrivenContext ctx) throws EJBException {
+
+  }
+
+  @Override
+  public void ejbRemove() throws EJBException {
+
+  }
+
+  @Override
+  public void onMessage(Message message) {
+
   }
 }
